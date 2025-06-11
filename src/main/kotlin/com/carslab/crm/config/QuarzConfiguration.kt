921db1f164940f06c80f33d5054c5d831468d5f6
MaciejSienkiewicz <maciej.sienkiewicz@allegro.pl>
2025-06-11 14:51:51 +0200

    quartz

--- src/main/kotlin/com/carslab/crm/config/QuarzConfiguration.kt
+++ src/main/kotlin/com/carslab/crm/config/QuarzConfiguration.kt
@@ -1,26 +1,31 @@
 package com.carslab.crm.config
 
 import com.carslab.crm.modules.schedulers.DailyAbandonedVisitsJob
-import org.quartz.CronScheduleBuilder
-import org.quartz.JobBuilder
-import org.quartz.JobDetail
-import org.quartz.Trigger
-import org.quartz.TriggerBuilder
+import org.quartz.*
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.boot.autoconfigure.quartz.QuartzDataSource
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.scheduling.annotation.EnableScheduling
+import java.util.*
+import javax.sql.DataSource
 
 @Configuration
 @EnableScheduling
 class QuartzConfiguration {
 
+    private val logger = LoggerFactory.getLogger(QuartzConfiguration::class.java)
+
     @Bean
     fun dailyAbandonedVisitsJobDetail(): JobDetail {
+        logger.info("Creating dailyAbandonedVisitsJobDetail bean")
         return JobBuilder.newJob(DailyAbandonedVisitsJob::class.java)
             .withIdentity("dailyAbandonedVisitsJob", "cleanupJobs")
             .withDescription("Daily cleanup of abandoned visits from previous day")
             .storeDurably(true)
+            .requestRecovery(true)
             .build()
     }
 
@@ -28,13 +33,18 @@ class QuartzConfiguration {
     fun dailyAbandonedVisitsTrigger(
         @Qualifier("dailyAbandonedVisitsJobDetail") jobDetail: JobDetail
     ): Trigger {
+        logger.info("Creating dailyAbandonedVisitsTrigger bean")
+
+        // Zacznij trigger za 2 minuty od teraz, żeby dać czas na pełne uruchomienie aplikacji
+        val startTime = Date(System.currentTimeMillis() + 120000) // +2 minuty
+
         return TriggerBuilder.newTrigger()
             .forJob(jobDetail)
             .withIdentity("dailyAbandonedVisitsTrigger", "cleanupJobs")
             .withDescription("Trigger for daily abandoned visits cleanup")
+            .startAt(startTime)
             .withSchedule(
-                // ✅ POPRAWIONY Cron: "0 0 1 * * ?" (co dzień o 1:00)
-                CronScheduleBuilder.cronSchedule("0 0 1 * * ?")
+                CronScheduleBuilder.cronSchedule("0 0 1 * * ?") // Codziennie o 1:00
                     .withMisfireHandlingInstructionDoNothing()
             )
             .build()
