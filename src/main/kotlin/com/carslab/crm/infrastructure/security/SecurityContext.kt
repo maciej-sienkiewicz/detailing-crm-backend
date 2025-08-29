0acef30a84963715083295f473bf801a2c162b84
MaciejSienkiewicz <maciej.sienkiewicz@allegro.pl>
2025-08-29 20:06:52 +0200

    clean obsolete code

--- src/main/kotlin/com/carslab/crm/infrastructure/security/SecurityContext.kt
+++ src/main/kotlin/com/carslab/crm/infrastructure/security/SecurityContext.kt
@@ -1,7 +1,6 @@
 package com.carslab.crm.infrastructure.security
 
 import com.carslab.crm.infrastructure.persistence.entity.UserEntity
-import com.carslab.crm.modules.visits.infrastructure.events.CurrentUser
 import com.carslab.crm.security.UserPrincipal
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Component
@@ -31,24 +30,6 @@ class SecurityContext {
         return when (val principal = authentication?.principal) {
             is UserEntity -> principal.username
             else -> null
-        }
-    }
-
-
-    fun getCurrentUser(): CurrentUser {
-        val authentication = SecurityContextHolder.getContext().authentication
-        return when (val principal = authentication?.principal) {
-            is UserPrincipal -> CurrentUser(
-                id = principal.id.toString(),
-                name = principal.username,
-                companyId = principal.companyId
-            )
-            is UserEntity -> CurrentUser(
-                id = principal.id.toString(),
-                name = principal.username,
-                companyId = principal.companyId
-            )
-            else -> throw IllegalStateException("No authenticated user found")
         }
     }
 }
--- src/main/kotlin/com/carslab/crm/modules/visits/infrastructure/processor/dto/MediaUploadData.kt
+++
@@ -1,9 +0,0 @@
-package com.carslab.crm.modules.visits.infrastructure.processor.dto
-
-import com.carslab.crm.modules.visits.domain.valueobjects.MediaMetadata
-import org.springframework.web.multipart.MultipartFile
-
-data class MediaUploadData(
-    val fileData: MultipartFile,
-    val metadata: MediaMetadata
-)
