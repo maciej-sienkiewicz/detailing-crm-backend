3346b6e4bf7b8e88ec28edbf473beee6a3606e25
Krancki <kr.przygocki@gmail.com>
2025-09-01 01:23:37 +0200

    update

--- src/main/kotlin/com/carslab/crm/modules/signature/config/WebSocketConfig.kt
+++ src/main/kotlin/com/carslab/crm/modules/signature/config/WebSocketConfig.kt
@@ -22,7 +22,6 @@ class WebSocketConfig(
         logger.info("Registering WebSocket handlers...")
 
         registry.addHandler(signatureWebSocketHandler, "/ws/tablet/{deviceId}", "/ws/workstation/{workstationId}")
-            .setAllowedOriginPatterns("*")
             .addInterceptors(WebSocketHandshakeInterceptor())
 
         logger.info("WebSocket handlers registered successfully")
